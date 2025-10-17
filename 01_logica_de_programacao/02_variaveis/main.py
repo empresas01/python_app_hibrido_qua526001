6405325c082b16e9c5d58500983d3ece9e130668
empresas01 <empresaszero1@gmail.com>
2025-10-17 16:41:09 -0300

    Meu primeiro commit. Aula do dia 17/10/2025.

---
+++ 01_logica_de_programacao/02_variaveis/main.py
@@ -0,0 +1,11 @@
+#
+nome_completo = "Lucas Ferreira" # string
+idade = 30 #  
+altura = 1.70 #
+Programador = True # bool (boolean)
+
+
+print (nome_completo)
+print (idade)
+print (altura)
+print (Programador)    
---
+++ 01_logica_de_programacao/03_tipos_de_dados/mayn.py
@@ -0,0 +1,11 @@
+# declaração de variável
+nome = "Lucas Ferreira" 
+idade = 30 
+altura = 1.70 
+programador = True # bool (booleano)
+
+# imprimindo na tela
+print(nome) 
+print(idade)
+print(altura)
+print(programador)
