33f6ed32e138ed7b3a9e5a3f64d740b4e30f9dfb
empresas01 <empresaszero1@gmail.com>
2025-11-28 17:50:26 -0300

    Aula 28/11/25

--- 07_App_hibrido/04_flet_fuel/src/main.py
+++ 07_App_hibrido/04_flet_fuel/src/main.py
@@ -2,6 +2,31 @@ import flet as ft
 
 
 def main(page: ft.Page):
+
+    def calcular_combustivel(e):
+        if not gasolina.value:
+            gasolina.error_text = "Valor da gasolina não pode ficar vazio."
+            gasolina.update()
+        else:
+            gasolina.error_text = ""
+            gasolina.update()
+        if not etanol.value:
+            etanol.error_text = "Valor do etanol não pode ficar vazio."
+            etanol.update()
+        else:
+            etanol.error_text = ""
+
+            gasolina.value = float(gasolina.value.replace(",", "."))
+            etanol.value = float(etanol.value.replace(",", "."))
+            resultado = "Gasolina" if (gasolina.value * 0.7) < etanol.value else "Etanol"
+            dlg_modal.content.value = resultado
+            gasolina.value = ""
+            etanol.value = ""
+
+            page.open(dlg_modal)  
+
+            page.update()
+                
     page.title = "App Flex Fuel"
     page.scroll = "adaptive"
     page.theme_mode = ft.ThemeMode.LIGHT
@@ -14,7 +39,8 @@ def main(page: ft.Page):
     etanol = ft.TextField(
         label="Preço do Etanol",
         prefix_text="R$ ",
-        keyboard_type=ft.KeyboardType.NUMBER
+        keyboard_type=ft.KeyboardType.NUMBER,
+        on_submit=calcular_combustivel
     )
     dlg_modal = ft.AlertDialog(
         modal=True,
@@ -23,12 +49,12 @@ def main(page: ft.Page):
         actions=[ft.TextButton("OK", on_click=lambda e:page.close(dlg_modal))],
         actions_alignment=ft.MainAxisAlignment.END
     )
-    
     page.add(
         ft.SafeArea(
             ft.Container(
                 ft.Text("FLEX FUEL", size=30, weight="bold"),
                 alignment=ft.alignment.center,
+                padding=90
             ),
             # expand=True,
         ),
@@ -36,8 +62,18 @@ def main(page: ft.Page):
             [ 
                 ft.Container(gasolina, col={"sm": 6, "md": 4, "xl": 2}),
                 ft.Container(etanol, col={"sm": 6, "md": 4, "xl": 2}),
-            ]
-        )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+       ),
+       ft.Row(
+           [
+               ft.Container(
+                   ft.ElevatedButton("Calcular", on_click=calcular_combustivel),
+                    padding=30                      
+               )
+           ],
+           alignment=ft.MainAxisAlignment.CENTER
+       )
     )
 
 
